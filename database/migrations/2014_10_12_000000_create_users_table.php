93445eaca589195ae98ce1814880f3dbd23f9750
Rifki Pratama <yudarifki19@gmail.com>
2021-10-05 00:15:21 +0700

    update migrate user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,6 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable(false);
             $table->string('phone_number')->nullable(false);
-            $table->string('photo_profile')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
