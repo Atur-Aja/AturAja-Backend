202f73cccf843c1fec190eb44dc4c351a8139b2c
primayasa <75405558+primayasa@users.noreply.github.com>
2021-10-04 17:26:32 +0700

    Add database migrations

---
+++ database/migrations/2021_10_04_000301_create_friends_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('first_user_id');
+            $table->unsignedBigInteger('second_user_id');            
+            $table->timestamps();
+            $table->foreign('first_user_id')->references('id')->on('users');
+            $table->foreign('second_user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
