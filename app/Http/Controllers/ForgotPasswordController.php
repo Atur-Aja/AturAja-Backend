0bf67175313ed2a4d8fb3cb64481bb38e571832d
primayasa <75405558+primayasa@users.noreply.github.com>
2021-12-21 21:28:07 +0700

    update forgot password request validation

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
 use App\Rules\IsValidPassword;
 
 class ForgotPasswordController extends Controller
 {
     public function forgot(){
-        $credentials = request()->validate(['email' => 'required|email']);
+        // Validate Request
+        $validator = Validator::make(request()->all(), [            
+            'email' => 'required|email|exists:App\Models\User,email',
+        ]);
 
+        if($validator->fails()) {
+            return response()->json($validator->messages());
+        }
+
+        $credentials = request()->only(['email']);
         Password::sendResetLink($credentials);
 
         return response()->json([
@@ -19,11 +28,18 @@ class ForgotPasswordController extends Controller
     }
 
     public function reset(){
-        $credentials = request()->validate([
-            'email' => 'required|email',
+        // Validate Request
+        $validator = Validator::make(request()->all(), [            
+            'email' => 'required|email|exists:App\Models\User,email',
             'password' => ['required', new isValidPassword(), 'confirmed'],
             'token' => 'required|string'
         ]);
+
+        if($validator->fails()) {
+            return response()->json($validator->messages());
+        }
+
+        $credentials = request()->only(['email', 'password', 'password_confirmation', 'token']);
 
         $email_password_status = Password::reset($credentials, function ($user, $password){
             $user->password = app('hash')->make($password);
