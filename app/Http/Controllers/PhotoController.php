f265f4eac609870877446d9b18844d4acd6a570f
Rifki Pratama <yudarifki19@gmail.com>
2022-01-01 14:06:27 +0700

    change photo controller bug

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 
 class PhotoController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('jwt.verify');
-    }
-
     public function image($fileName){
         return response()->download(public_path().'/image/'.$fileName, 'photo');
     }
